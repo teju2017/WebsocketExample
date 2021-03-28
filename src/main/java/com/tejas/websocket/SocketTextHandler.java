c006c567095e4e72811e2ba6b51f9cd50c80d191
teju <teju.hm@practice.com>
2021-03-28 23:58:06 +0800

    WebSocketExample

---
+++ src/main/java/com/tejas/websocket/SocketTextHandler.java
@@ -0,0 +1,25 @@
+package com.tejas.websocket;
+
+import java.io.IOException;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class SocketTextHandler extends TextWebSocketHandler {
+	
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message)
+			throws InterruptedException, IOException {
+
+		String payload = message.getPayload();
+		JSONObject jsonObject = new JSONObject(payload);
+		System.out.println("Payload is ="+payload.toString());
+		session.sendMessage(new TextMessage("Hi " + jsonObject.get("user") + " how may we help you?"));
+	}
+
+
+}
---
+++ src/test/java/com/tejas/websocket/WebsocketExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejas.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
